07c40a418e3ffa7e080b4102e5f744b0a8c80ff7
Stanley Zheng <dev.stanzheng@gmail.com>
2017-03-10 23:12:47 -0500

    updating functionality

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
+var Fuse = require('fuse.js');
+var emojis = require('./data/emoji.json');
+
 module.exports = (input, opts) => {
 	if (typeof input !== 'string') {
 		throw new TypeError(`Expected a string, got ${typeof input}`);
 	}
 
-	opts = opts || {};
+	opts = opts || {
+		keys: ['description']
+	};
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	var fuse = new Fuse(emojis, opts);
+
+	return fuse.search(input).slice(0, 5);
 };
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
 import fn from '.';
 
-test('title', t => {
-	const err = t.throws(() => fn(123), TypeError);
-	t.is(err.message, 'Expected a string, got number');
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+test('find description', t => {
+	t.is(fn('switzerland')[0].description, 'Switzerland');
+	t.is(fn('melon')[0].description, 'melon');
+	t.is(fn('palette')[0].description, 'artist palette');
 });
