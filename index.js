79e221d778f76c677361a8ec1ed1cd47db42d865
Stanley Zheng <dev.stanzheng@gmail.com>
2017-03-11 08:00:12 -0500

    fix indenting

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ var Fuse = require('fuse.js');
 var emojis = require('./data/emoji.json');
 
 module.exports = {
-	emojis: emojis,
-	search: search
+  emojis: emojis,
+  search: search
 };
 
 function search(input, opts) {
-	if (typeof input !== 'string') {
-		// throw new TypeError(`Expected a string, got ${typeof input}`);
-		return `Expected a string, got ${typeof input}...`;
-	}
+  if (typeof input !== 'string') {
+    // throw new TypeError(`Expected a string, got ${typeof input}`);
+    return `Expected a string, got ${typeof input}.`;
+  }
 
-	opts = opts || {
-		keys: ['description']
-	};
+  opts = opts || {
+    keys: ['description']
+  };
 
-	var fuse = new Fuse(emojis, opts);
+  var fuse = new Fuse(emojis, opts);
 
-	return fuse.search(input).slice(0, 5);
+  return fuse.search(input).slice(0, 5);
 }
